d372034bcbd35644a0cca9bf0017a4568ef908ff
Ardana Firmansyah <79956187+danzlan12@users.noreply.github.com>
2021-09-01 21:41:52 +0700

    commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link href="wow.css" rel="stylesheet" type="text/css" />
+
+<svg viewBox="0 0 1800 600">
+  <symbol id="s-text">
+    <text text-anchor="middle"
+          x="50%"
+          y="68%"
+          class="webcoderskull"
+          >
+      ARDANA         
+    </text>
+    <text text-anchor="middle"
+          x="50%"
+          y="68%"
+          class="text--line"
+          >
+     FIRMANSYAH
+    </text>
+    
+  </symbol>
+  
+  <g class="g-ants">
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+    <use xlink:href="#s-text"
+      class="webcoderskull-1"></use>     
+  </g>
+  
+  
+</svg>
